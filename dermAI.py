380f2226b887605220d9f7f20a3d09eca109abef
Hisham ul hakeem A <155228704+Hishamulhakeem@users.noreply.github.com>
2025-12-14 17:30:43 +0530

    Add files via upload

--- dermAI.py
+++ dermAI.py
@@ -93,3 +93,66 @@ history1 = model1.fit(
     steps_per_epoch=train_gen.samples//train_gen.batch_size,
     validation_steps=validation_gen.samples//validation_gen.batch_size
 )
+
+history2 = model2.fit(
+    train_gen,
+    epochs=10,
+    validation_data=validation_gen,
+    steps_per_epoch=train_gen.samples//train_gen.batch_size,
+    validation_steps=validation_gen.samples//validation_gen.batch_size
+)
+
+model2.save('model.h5')
+
+from tensorflow.keras.preprocessing import image
+import numpy as np
+import matplotlib.pyplot as plt
+
+IMG_SIZE = (224, 224)   
+img_path = "test/image4.jpeg"
+
+class_labels = [
+    "Acne",
+    "Actinic_Keratosis",
+    "Benign_tumors",
+    "Bullous",
+    "Candidiasis",
+    "DrugEruption",
+    "Eczema",
+    "Infestations_Bites",
+    "Lichen",
+    "Lupus",
+    "Moles",
+    "Psoriasis",
+    "Rosacea",
+    "Seborrh_Keratoses",
+    "SkinCancer",
+    "Sun_Sunlight_Damage",
+    "Tinea",
+    "Unknown_Normal",
+    "Vascular_Tumors",
+    "Vasculitis",
+    "Vitiligo",
+    "Warts"
+]
+
+img = image.load_img(img_path, target_size=IMG_SIZE)
+img_array = image.img_to_array(img)
+img_array = np.expand_dims(img_array, axis=0)
+img_array = img_array / 255.0
+
+prediction = model2.predict(img_array, verbose=0)
+predicted_index = np.argmax(prediction)
+predicted_class = class_labels[predicted_index]
+confidence = prediction[0][predicted_index] * 100
+
+plt.imshow(img)
+plt.title(f"Predicted: {predicted_class} ({confidence:.2f}%)")
+plt.axis("off")
+plt.show()
+
+train_acc = history2.history['accuracy'][-1]
+val_acc = history2.history['val_accuracy'][-1]
+
+print(f"Final Training Accuracy: {train_acc*100:.2f}%")
+print(f"Final Validation Accuracy: {val_acc*100:.2f}%")
